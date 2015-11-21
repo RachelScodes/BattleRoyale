ff29d289bebeb54ef9c0d399f152ddf038ed74fd
Mala <m.nimalasuriya@gmail.com>
2015-11-21 11:44:23 -0500

    connected database and hit test route

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict'
+///// require express packages /////////////////////////////////////////////////
+let express = require('express');
+let app = express();
+let expressJWT = require('express-jwt');
+let bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+let logger = require('morgan');
+let path = require('path');
+let io = require('socket.io');
+// let routes = require('./controllers/ add file name here!!!' );
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use('/', express.static(__dirname + '/public'));
+
+///// connect database /////////////////////////////////////////////////////////
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/battle-royale');
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (callback) => {
+  console.log('db connected!');
+});
+
+///// write routes below ///////////////////////////////////////////////////////
+app.get('/', function(req, res){ // test route // delete later
+  res.send('hit test route!');
+});
+
+
+
+
+
+
+
+
+
+///// server ///////////////////////////////////////////////////////////////////
+let server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('server running!');
+});
