a556e7545725c5f4b52a0263942f7fcc7e5222fd
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-22 14:51:57 -0500

    adding style, added foter to html with links to github

--- public/game.js
+++ public/game.js
@@ -1,1 +1,16 @@
 ///// trivia game mechanics ////////////////////////////////////////////////////
+
+var app = $('#app');          var game = $('#game');
+
+// empty lobby, chat, and user-list
+// show loading graphic/prompt
+function loading() {
+   $('#lobby').empty()
+   $('#chat-window').empty()
+   $('#user-list').empty()
+
+   // draw load screen
+}
+
+// draw the clock
+// draw the game
