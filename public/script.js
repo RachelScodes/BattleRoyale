ba8376b938f0c2d1b9c65d35cae0d75f2c1526b8
Mala <m.nimalasuriya@gmail.com>
2015-11-23 13:29:11 -0500

    created script.js file, added first ajax call to get trivia questions, added API parsing function to server.js file, added get question button to index.html

---
+++ public/script.js
@@ -0,0 +1,15 @@
+$('.get-question').click(function(event){
+  event.preventDefault();
+  console.log('clicked!');
+
+  $.ajax({
+    url: 'https://pareshchouhan-trivia-v1.p.mashape.com/v1/getAllQuizQuestions?limit=10&page=1',
+    headers:{'X-Mashape-Key': 'lX0iJk5iGlmshE7fTOCtRf7hsj3Zp1NuuosjsnBKj4jIROPs9R'}
+  }).done(function(data){
+        console.log(data);
+    var list = $('.selected-question').append('<ul class="current-question">').find('ul');
+    for (var i = 0; i < data.length; i ++) {
+      $(list).append('<li>' + data[i] + '</li>');
+    };
+  });
+});
