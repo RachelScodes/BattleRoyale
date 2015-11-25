45847f4bcafdd76735382f4bb2ccdad0cfa9d55e
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-24 21:26:44 -0500

    changed server for heroku

--- server.js
+++ server.js
@@ -91,4 +91,6 @@ io.on('connection', function(client) {
     });
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
