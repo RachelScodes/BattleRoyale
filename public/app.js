cda9be0684cdcd11c42015d74f14815dffdf9104
Anita Growsoon <anitagrowsoon@gmail.com>
2015-11-28 23:46:26 -0500

    styling is legit a mess

--- public/app.js
+++ public/app.js
@@ -7,20 +7,22 @@ var socket = io(), ///// listens and emits client-side - meat of socket in backe
 
 $(function() {
 	//these divs will be hidden at the start
-	$('#loginpage').hide();
+	// $('#loginpage').hide();
 	$('#createuserpage').hide();
-  $('#authenticate-page').hide(); /////
+   $('#authenticate-page').hide(); /////
+   $('.loginform').hide()
+   $('#game').hide()
 
 	$('.login-link').click(function() {
-		$('#navigation').hide();
+		$('#links').hide();
 		// $('#loginpage').show(); ///// commented out to render authenticate login and test
-    $('#authenticate-page').show(); ///// added to render authenticate login and test
-  });
+      $('#authenticate-page').show(); ///// added to render authenticate login and test
+   });
 
 
 /// create new user - POST http://localhost:3000/user/signup ///////////////////////
 $('.createuser-link').click(function() {
-  $('#navigation').hide();
+  $('#links').hide();
   $('#createuserpage').show();
 });
 
@@ -41,66 +43,69 @@ $('#create-user-submit-button').click(function() {
 });
 
 
-  var goToLogin = function() {
-    $('#createuserpage').hide();
-    $('#authenticate-page').show();
-    $('#submit-login').show(); ///// add login submit button to index.html
-  }
+   var goToLogin = function() {
+      $('#createuserpage').hide();
+      $('#authenticate-page').show();
+      $('#authenticate-submit-login').show(); ///// add login submit button to index.html
+   }
 
 //////// authenticate - POST http://localhost:3000/user/authenticate ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  $('#authenticate-submit-button').click(function() {
-    // console.log('clicked');
-    var username = $('#authenticate-username-input').val();
-    var password = $('#authenticate-password-input').val();
-    var userLoginData = {
-      username: username,
-      password: password
-    }
-    // console.log(userLoginData);
-    $.ajax({ ///// executes user_controller.js auth function
-      url: "/user/authenticate",
-      method: "POST",
-      data: userLoginData
-    }).done(goToLobby)
+   $('#authenticate-submit-button').click(function() {
+      // console.log('clicked');
+      var username = $('#authenticate-username-input').val();
+      var password = $('#authenticate-password-input').val();
+      var userLoginData = {
+         username: username,
+         password: password
+      }
+      // console.log(userLoginData);
+      $.ajax({ ///// executes user_controller.js auth function
+         url: "/user/authenticate",
+         method: "POST",
+         data: userLoginData
+      }).done(goToLobby)
 
-    // console.log(username + 'this should be username');
+      // console.log(username + 'this should be username');
       myUser = username;
       socket.emit('add user', username);
       $('#authenticate-username-input').val('');
-    }); // end of authenticate submit button jQuery call
+   }); // end of authenticate submit button jQuery call
 
     var goToLobby = function() {
       $('#authenticate-page').hide();
+      $('#game').show()
+      $('#vid-div').hide()
+      $('loginpage').css('z-index','-1')
     }
 
 
-	$('#message').keypress(function(event) {
+	$('#compose').keypress(function(event) {
 		if(event.keyCode === 13) {
-			var message = $('#message').val();
-      console.log(myUser);
+			var compose = $('#compose').val();
+         console.log(myUser);
 
 			socket.emit('send message', {name: myUser, message: message});
-			$('#message').val('');
+			$('#compose').val('');
 		}
 	});
 
 
-// SOCKET EVENTS
+   // SOCKET EVENTS
 
-socket.on('user joined', function(users) {
-	var usersList = $('#users ul');
-	usersList.empty();
-	users.forEach(function(user) {
-		var userElement = $('<li>');
-		userElement.text(user.name);
-		usersList.append(userElement);
-	});
-});
+   socket.on('user joined', function(users) {
+   	var usersList = $('#usernames');
+   	usersList.empty();
+   	users.forEach(function(user) {
+   		var userElement = $('<li>');
+   		userElement.text(user.name);
+   		usersList.append(userElement);
+   	});
+   });
 
-socket.on('send message', function(data) {
-	var chatList = $('#messages');
-	var message = $('<li>');
-	message.text(data.name + " : " + data.message);
-	chatList.append(message);
-});
+   socket.on('send message', function(data) {
+   	var chatList = $('#messages');
+   	var message = $('<li>');
+   	message.text(data.name + " : " + data.message);
+   	chatList.append(message);
+   });
 })
