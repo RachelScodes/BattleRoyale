8cb4ff22f6b295809781cb02d894365fc8ab6ae9
Mala Nimalasuriya <m.nimalasuriya@gmail.com>
2015-11-30 00:21:28 -0500

    Revert "Merge develop onto master for Heroku redeployment."

--- config.js
+++
@@ -1,8 +0,0 @@
-///// once authentication is working
-///// pull out secret from user_controller.js, place that info in this file, and require the config in server.js
-'use strict'
-
-module.exports = {
-  'secret': 'thissecretissofetch',
-  'database': process.env.MONGOLAB_URI || 'mongodb://localhost/battle-royale'
-};
